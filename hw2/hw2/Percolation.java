8fd4cd591f2bf1073d1a2e9e6660de1dd7ebba3e
Min <changminapiao@lawn-143-215-59-12.lawn.gatech.edu>
2020-10-28 18:08:33 -0400

    double sets

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -7,30 +7,35 @@ import java.io.IOException;
 public class Percolation {
     public boolean[][] perc;
     WeightedQuickUnionUF set;
+    WeightedQuickUnionUF tract;
     public int top;
     public int bottom;
     public int opensites=0;
     public int length;
 
+
     public Percolation(int N) {
+        if(N<=0){
+            throw new IllegalArgumentException();
+        }
             perc = new boolean[N][N];
             set = new WeightedQuickUnionUF(N*N+2);
+            tract = new WeightedQuickUnionUF(N*N+2);
             top = N*N+1;
             bottom = N*N;
             length = N;
     }
     public void open(int row, int col) {
         if (perc[col][row]==false) {
-            if (col < perc.length && row == 0) {
+            if (row == 0) {
                 perc[col][row] = true;
                 set.union(top, index(row, col));
+                tract.union(top,index(row, col));
                 connect(row, col);
-            } else if (col < perc.length && row == perc.length - 1) {
+            } else if (row == perc.length - 1) {
                 perc[col][row] = true;
                 connect(row, col);
-                if (set.connected(top,index(row,col))) {
-                    set.union(bottom, index(row, col));
-                }
+                set.union(bottom, index(row, col));
             } else {
                 perc[col][row] = true;
                 connect(row, col);
@@ -45,7 +50,7 @@ public class Percolation {
         return false;
     }
     public boolean isFull(int row, int col){
-        if (set.connected(top,index(row,col))){
+        if (tract.connected(top,index(row,col))){
             return true;
         }
         return false;
@@ -54,10 +59,7 @@ public class Percolation {
         return opensites;
     }
     public boolean percolates(){
-        if (set.connected(top,bottom)){
-            return true;
-        }
-        return false;
+        return (set.connected(top,bottom));
     }
 
     private int index(int y,int x){
@@ -68,25 +70,36 @@ public class Percolation {
         if (row != perc.length-1) {
             if (isOpen(row + 1, col)) {
                 set.union(index(row + 1, col), index(row, col));
+                tract.union(index(row + 1, col), index(row, col));
             }
         }
         if (row != 0) {
             if (isOpen(row - 1, col)) {
                 set.union(index(row - 1, col), index(row, col));
+                tract.union(index(row - 1, col), index(row, col));
             }
         }
         if (col != perc.length-1) {
             if (isOpen(row, col + 1)) {
                 set.union(index(row, col + 1), index(row, col));
+                tract.union(index(row, col + 1), index(row, col));
+
             }
         }
         if (col != 0) {
             if (isOpen(row, col - 1)) {
                 set.union(index(row , col-1), index(row, col));
+                tract.union(index(row , col-1), index(row, col));
             }
         }
     }
 
+    public static void main(String arg[]){
+        Percolation perc = new Percolation(2);
+        perc.open(1,0);
+        perc.open(0,0);
+        System.out.println(perc.percolates());
+    }
 
 
 
