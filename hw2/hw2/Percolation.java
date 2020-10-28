fa49eba9a2c8e4829b0961f8505ab3ee913f93e8
Min <changminapiao@lawn-143-215-59-12.lawn.gatech.edu>
2020-10-28 16:22:47 -0400

    first version

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,94 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.io.IOException;
+
 public class Percolation {
+    public boolean[][] perc;
+    WeightedQuickUnionUF set;
+    public int top;
+    public int bottom;
+    public int opensites=0;
+    public int length;
+
+    public Percolation(int N) {
+            perc = new boolean[N][N];
+            set = new WeightedQuickUnionUF(N*N+2);
+            top = N*N+1;
+            bottom = N*N;
+            length = N;
+    }
+    public void open(int row, int col) {
+        if (perc[col][row]==false) {
+            if (col < perc.length && row == 0) {
+                perc[col][row] = true;
+                set.union(top, index(row, col));
+                connect(row, col);
+            } else if (col < perc.length && row == perc.length - 1) {
+                perc[col][row] = true;
+                connect(row, col);
+                if (set.connected(top,index(row,col))) {
+                    set.union(bottom, index(row, col));
+                }
+            } else {
+                perc[col][row] = true;
+                opensites++;
+                connect(row, col);
+            }
+        }
+    }
+    public boolean isOpen(int row, int col){
+        if (perc[col][row]==true){
+            return true;
+        }
+        return false;
+    }
+    public boolean isFull(int row, int col){
+        if (set.connected(top,index(row,col))){
+            return true;
+        }
+        return false;
+    }
+    public int numberOfOpenSites()  {
+        return opensites;
+    }
+    public boolean percolates(){
+        if (set.connected(top,bottom)){
+            return true;
+        }
+        return false;
+    }
+
+    private int index(int y,int x){
+        return (y*length)+x;
+    }
+
+    private void connect(int row,int col){
+        if (row != perc.length-1) {
+            if (isOpen(row + 1, col)) {
+                set.union(index(row + 1, col), index(row, col));
+            }
+        }
+        if (row != 0) {
+            if (isOpen(row - 1, col)) {
+                set.union(index(row - 1, col), index(row, col));
+            }
+        }
+        if (col != perc.length-1) {
+            if (isOpen(row, col + 1)) {
+                set.union(index(row, col + 1), index(row, col));
+            }
+        }
+        if (col != 0) {
+            if (isOpen(row, col - 1)) {
+                set.union(index(row , col-1), index(row, col));
+            }
+        }
+    }
+
+
+
+
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,51 @@
 package hw2;
-
+import edu.princeton.cs.introcs.StdRandom;
 public class PercolationStats {
+    public double sum_fraction=0;
+    public int experiment_num;
+    public double[] t;
+    public double mean;
+    public double Std;
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        experiment_num = T;
+        t = new double[T];
+        Percolation perc = pf.make(N);
+        for (int i = 0;i<T;i++){
+            while (!perc.percolates()){
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                perc.open(row,col);
+            }
+            double fraction = (double)perc.numberOfOpenSites()/(N*N);
+            t[i] = fraction;
+            sum_fraction+=fraction;
+        }
+    }
+
+    public double mean(){
+        mean = sum_fraction/experiment_num;
+        return mean;
+    }
+
+    public double stddev() {
+        double sum_std=0;
+        for (int i = 0;i<t.length;i++){
+            sum_std += (t[i]-mean)*(t[i]-mean);
+        }
+        double std = Math.sqrt(sum_std/(experiment_num-1));
+        Std = std;
+        return Std;
+    }
+
+    public double confidenceLow(){
+        double confidencelow = mean - ((1.96*Std)/Math.sqrt(experiment_num));
+        return confidencelow;
+    }
+
+    public double confidenceHigh(){
+        double confidencehigh = mean + ((1.96*Std)/Math.sqrt(experiment_num));
+        return confidencehigh;
+    }
+
 
 }
