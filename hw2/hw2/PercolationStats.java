6a14d5b90a7214d1be40b35e1ed0eeb0d7ac78d5
Min <changminapiao@lawn-143-215-59-12.lawn.gatech.edu>
2020-10-29 14:04:32 -0400

    std

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,51 +1,57 @@
 package hw2;
 import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 public class PercolationStats {
-    public double sum_fraction=0;
+    public double sum_fraction = 0;
     public int experiment_num;
     public double[] t;
     public double mean;
     public double Std;
-    public PercolationStats(int N, int T, PercolationFactory pf){
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N<=0||T<=0){
+            throw new IllegalArgumentException();
+        }
         experiment_num = T;
         t = new double[T];
-        for (int i = 0;i<T;i++){
+        for (int i = 0; i < T; i++) {
             Percolation perc = pf.make(N);
-            while (!perc.percolates()){
+            while (!perc.percolates()) {
                 int row = StdRandom.uniform(N);
                 int col = StdRandom.uniform(N);
-                perc.open(row,col);
+                perc.open(row, col);
             }
-            double fraction = (double)perc.numberOfOpenSites()/(N*N);
+            double fraction = (double) perc.numberOfOpenSites() / (N * N);
             t[i] = fraction;
-            sum_fraction+=fraction;
+            sum_fraction += fraction;
         }
     }
 
-    public double mean(){
-        mean = sum_fraction/experiment_num;
+    public double mean() {
+        mean = sum_fraction / experiment_num;
         return mean;
     }
 
     public double stddev() {
-        double sum_std=0;
-        for (int i = 0;i<t.length;i++){
-            sum_std += (t[i]-mean)*(t[i]-mean);
-        }
-        double std = Math.sqrt(sum_std/(experiment_num-1));
-        Std = std;
+        Std = StdStats.stddev(t);
         return Std;
     }
 
-    public double confidenceLow(){
-        double confidencelow = mean - ((1.96*Std)/Math.sqrt(experiment_num));
+    public double confidenceLow() {
+        double confidencelow = mean - ((1.96 * Std) / Math.sqrt(experiment_num));
         return confidencelow;
     }
 
-    public double confidenceHigh(){
-        double confidencehigh = mean + ((1.96*Std)/Math.sqrt(experiment_num));
+    public double confidenceHigh() {
+        double confidencehigh = mean + ((1.96 * Std) / Math.sqrt(experiment_num));
         return confidencehigh;
     }
 
+    public static void main(String arg[]) {
+        PercolationFactory pf = new PercolationFactory();
+        PercolationStats a = new PercolationStats(20, 10, pf);
+        System.out.println(a.stddev());
 
+
+    }
 }
