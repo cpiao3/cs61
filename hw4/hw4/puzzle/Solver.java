ab930ebc0b6b312e1c382885d5b22b92f26fc17c
Min <changminapiao@lawn-143-215-59-12.lawn.gatech.edu>
2020-11-04 20:56:49 -0500

    final

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -55,7 +55,22 @@ public class Solver {
         }
         return solution;
     }
-
+    private class SearchNode implements Comparable<SearchNode>{
+        public WorldState word;
+        public int moves;
+        public SearchNode prev;
+        public int priority;
+        public SearchNode(WorldState word, int moves, SearchNode prev){
+            this.word = word;
+            this.prev = prev;
+            this.moves = moves;
+            priority = this.moves + word.estimatedDistanceToGoal();
+        }
+        @Override
+        public int compareTo(SearchNode o2){
+            return this.priority-o2.priority;
+        }
+    }
 
 
 
